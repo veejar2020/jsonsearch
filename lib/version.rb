76070a33ede3b1dcf36c799d5a3408bc7568d87d
veejar2020 <veejar2020@gmail.com>
2018-02-06 00:01:40 -0800

    removed unwanted lib files

--- lib/version.rb
+++
@@ -1,3 +0,0 @@
-module Zensearch
-  VERSION = '0.0.1'.freeze
-end
--- lib/zensearch.rb
+++
@@ -1,4 +0,0 @@
-require 'version'
-require 'search_json'
-# Add requires for other files you add to your project here, so
-# you just need to require this one file in your bin file
